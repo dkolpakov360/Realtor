7296e8342ade1d36ab49bef10b4aab03dd2de750
Dmitry Kolpakov <53349505+dkolpakov360@users.noreply.github.com>
2020-10-01 17:10:32 +0300

    Update dbConnect.php

--- Realtor/dbConnect.php
+++ Realtor/dbConnect.php
@@ -10,7 +10,7 @@ class dbConnect
 
     public function __construct()
     {
-        $_connect = new PDO('mysql:dbname=test;host=127.0.0.1', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+        $_connect = new PDO('mysql:dbname=realtor;host=127.0.0.1', 'root', 'root', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 
         $this->_connect = $_connect;
     }
